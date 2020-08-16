7fa46acff5e64bac8c3643fd7799714113f82fb5
leonevil <leongb66@gmail.com>
2020-08-16 12:01:17 -0300

    Consulta simples com @Query

--- src/main/java/com/example/course/workshopmongo/resources/PostResource.java
+++ src/main/java/com/example/course/workshopmongo/resources/PostResource.java
@@ -20,15 +20,15 @@ public class PostResource {
 
 	@Autowired
 	private PostService service;
-	
-	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public ResponseEntity<Post> findById(@PathVariable String id){
+
+	@RequestMapping(value="/{id}", method = RequestMethod.GET)
+ 	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
 	
-	@RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
-	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
+	@RequestMapping(value="/titlesearch", method = RequestMethod.GET)
+ 	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
 		text = URL.decodeParam(text);
 		List<Post> list = service.findByTitle(text);
 		return ResponseEntity.ok().body(list);
